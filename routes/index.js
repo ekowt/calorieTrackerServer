e68e0abf39c5fc388855cd05b54790c204c716b7
ekowt15 <38335773+ekowt@users.noreply.github.com>
2022-11-08 02:22:00 -0500

    first commit

--- routes/index.js
+++ routes/index.js
@@ -5,6 +5,34 @@ const fs = require('fs');
 const Joi = require('joi');
 const cors = require('cors');
 
+const { MongoClient } = require("mongodb");
+
+// Connection URI
+const uri =
+"mongodb://localhost:27017/";;
+
+// Create a new MongoClient
+const client = new MongoClient(uri);
+
+async function run() {
+  try {
+    // Connect the client to the server (optional starting in v4.7)
+    await client.connect();
+
+    // Establish and verify connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Connected successfully to server");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+run().catch(console.dir);
+
+
+
+  
+
 //an array of course objects with 2 attributes each
 const foods = [
   {id: 1, name: 'pizza', calories:500},
--- routes/users.js
+++ routes/users.js
@@ -6,4 +6,27 @@ router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
+router.post('/', function(req, res, next) {
+  res.send('post');
+});
+
+
+router.get('/:id',(req,res)=>
+{
+
+})
+
+router.patch('/',(req,res)=>
+{
+
+})
+
+router.delete('/:id',(req,res)=>
+{
+
+})
+
+
+
+
 module.exports = router;
